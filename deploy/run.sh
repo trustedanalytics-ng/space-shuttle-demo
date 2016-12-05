4fe7dded772135e0cb3f821d289876a5f6104041
Karol Zalewski <karol.zalewski@4zal.net>
2016-12-05 10:04:52 +0100

    DPNG-12118: Using Kafka from Gateway instance. (#26)

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar space-shuttle-demo.jar --server.port=80 --spring.profiles.active=dummy-input,dummy-scoring,influx-storage
+java -jar space-shuttle-demo.jar --server.port=80 --spring.profiles.active=kafka-input,dummy-scoring,influx-storage
